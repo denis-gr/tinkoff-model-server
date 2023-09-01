fa3fc682f720602472b43a38beb5da8cc2576ebb
Denis Grigoriev <denisgrigoriev04@ya.ru>
2023-09-01 14:26:54 +0300

    bug fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import json
+
 from fastapi import FastAPI, Body
 
 from model import LLM
-
 
 app = FastAPI()
 
@@ -9,5 +10,5 @@ model = LLM()
 
 @app.post("/complete")
 async def read_item(prompt: str = Body()):
-    return { "text": model.complete(prompt) }
+    return { "text": str(model.complete(json.loads(prompt))) }
 
